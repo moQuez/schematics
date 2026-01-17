3fae81d88ed21abf04ccdc0608d55c4ca0ad6c80
Guillermo Velasquez <guillermo.velasquez@gmail.com>
2026-01-17 21:22:06 +0100

    feat(nvim): toggle obsidian checkbox in insert

--- stow/nvim/.config/nvim-Lazyvim/lua/plugins/obsidian.lua
+++ stow/nvim/.config/nvim-Lazyvim/lua/plugins/obsidian.lua
@@ -2,6 +2,17 @@ return {
   "obsidian-nvim/obsidian.nvim",
   version = "*",
   ft = "markdown",
+  keys = {
+    {
+      "<C-l>",
+      function()
+        return require("obsidian").util.toggle_checkbox()
+      end,
+      mode = "i",
+      desc = "Toggle checkbox (insert)",
+      buffer = true,
+    },
+  },
   opts = {
     dir = "~/Development/TestVault",
     daily_notes = {
